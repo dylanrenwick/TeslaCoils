6ad97a16f21953ed272ec07cdc457ecf807bba1f
gunnerwolf <skidsirongenesisdev@gmail.com>
2016-07-27 04:06:53 +0100

    Fixed new tesla modid

--- src/main/java/com/skidsdev/teslacoils/utils/VersionInfo.java
+++ src/main/java/com/skidsdev/teslacoils/utils/VersionInfo.java
@@ -5,5 +5,5 @@ public class VersionInfo
 	public static final String ModId = "teslacoils";
 	public static final String ModName = "Tesla Coils";
 	public static final String Version = "v0.2.0.1";
-	public static final String Dependencies = "required-after:Tesla;";
+	public static final String Dependencies = "required-after:tesla;";
 }
