75de0cf2c2d3fd4a71933e5aa0563e167422fa39
skidsirongenesisdev@gmail.com <skidsirongenesisdev@gmail.com>
2016-12-03 15:54:47 +0000

    Update v0.2.3.0

--- src/main/java/com/skidsdev/teslacoils/utils/VersionInfo.java
+++ src/main/java/com/skidsdev/teslacoils/utils/VersionInfo.java
@@ -4,6 +4,6 @@ public class VersionInfo
 {
 	public static final String ModId = "teslacoils";
 	public static final String ModName = "Tesla Coils";
-	public static final String Version = "v0.2.1.0";
+	public static final String Version = "v0.2.3.0";
 	public static final String Dependencies = "required-after:tesla;";
 }
